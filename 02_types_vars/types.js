48fd98510ec536bd20ebcfad5dbf3eb1adb41e03
Anna Ferro <anna.ferro@hfilmuniversitaet.de>
2024-05-23 18:37:33 +0200

    add 02_types

---
+++ 02_types_vars/types.js
@@ -0,0 +1,25 @@
+
+// Different data types used impliciteyl in Javascript.
+
+// TODO: Figure out the counterparts in C++ and create or extend the given main_types.cpp accordingly.
+
+let integer = 100; // the counterpart in C++ is int
+let floatingPoint = 70.6; // the counterpart in C++ is float
+let exponential = 0.30e4;  // the counterpart in C++ is double
+
+// Strings
+let name = 'Creative Coding'; // the counterpart in C++ is std::string 
+let message = "Double quotes work too";
+
+// Arrays
+let items = [1, 2, 3, 4, 5]; // the counterpart in C++ is composed of two parts: first is the type of the item (int) and the second is the square brackets.
+let complexItems = [1, "hello world", 8.0, null, undefined, 2, 3]; // in C++ we don't have a direct counterpart for dynamic arrays
+
+// dynamic objects
+let dog = {
+    species: "animal",
+    height: 1.2,
+    weigth: 13.3,
+    age: 10
+};
+//  in C++ we don't have a direct counterpart for dynamic objects
