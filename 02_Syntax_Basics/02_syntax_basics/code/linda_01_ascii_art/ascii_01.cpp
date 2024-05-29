53dca7576b564f3a4669c32f39d1977fc61eac0e
Linmaas <linda.j.maas@gmail.com>
2024-05-29 12:04:19 +0200

    added folders for us

--- 02_Syntax_Basics/02_syntax_basics/code/linda_01_ascii_art/ascii_01.cpp
+++ 02_Syntax_Basics/02_syntax_basics/code/linda_01_ascii_art/ascii_01.cpp
@@ -5,32 +5,31 @@ This example requires additional functionality. We need the possibility to write
 to the console, organize the characters, and store the actual character values. To achieve all
 of this, we use
 
-iostream    > The I/O stream class and provides functionality
+iostream    > The I/O stream class and provides functionality 
                 to input and output data from and to the command line console
                 https://cplusplus.com/reference/istream/iostream/?kw=iostream
 vector      > The generic vector container that stores and organizes the data; in contrast to
                 classical arrays, vectors have the capability to change in size
                 https://cplusplus.com/reference/vector/vector/?kw=vector
-string      > The string class data type that takes care of combining sets of single characters
+string      > The string class data type that takes care of combining sets of single characters 
                 https://cplusplus.com/reference/string/string/?kw=string
 */
 
-#include <iostream>
+#include <iostream> 
 #include <vector>
 #include <string>
 
-// Function to print the ASCII art
-void printAsciiArt(const std::vector<std::string> &art)
-{
 
-    for (const std::string &line : art)
-    {
+// Function to print the ASCII art
+void printAsciiArt(const std::vector<std::string>& art) {
+
+    for (const std::string& line : art) {
         std::cout << line << std::endl;
     }
 }
 
-int main()
-{
+
+int main() {
     // Define the ASCII art
     std::vector<std::string> smileyFace = {
         "  *****  ",
@@ -39,7 +38,8 @@ int main()
         "*   ^   *",
         "* \\___/ *",
         " *     * ",
-        "  *****  "};
+        "  *****  "
+    };
 
     // Print the ASCII art
     printAsciiArt(smileyFace);
