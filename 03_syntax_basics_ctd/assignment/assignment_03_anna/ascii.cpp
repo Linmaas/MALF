4bc0ca089cc680c7bb9cb51e5d9003798b4f868b
Anna Ferro <anna.ferro@hfilmuniversitaet.de>
2024-05-30 16:58:39 +0200

    add letters combination function

--- 03_syntax_basics_ctd/assignment/assignment_03_anna/ascii.cpp
+++ 03_syntax_basics_ctd/assignment/assignment_03_anna/ascii.cpp
@@ -1,3 +1,4 @@
+#include "ascii.hpp"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -5,11 +6,25 @@
 #include <thread>
 #include <chrono>
 
-#include "mail.hpp"
-#include "ascii.hpp"
+//#include "mail.hpp"
+
 
 using namespace std;
 
-std::vector<string> asciiArt = {
-       "This will be our Ascii Art"
-    };
+void lettersCombination(){
+    std::string username;
+    std::cout << "What's your name?";
+    std::cin >> username;
+
+    char firstLetterUsername = username[0]; // Accessing the first character
+
+    std::string lover;
+    std::cout << "What's the name of your lover?";
+    std::cin >> lover;
+
+    char firstLetterLover = lover[0];
+
+    // Create strings from individual characters
+    std::string combination = std::string(1, firstLetterUsername) + "+" + std::string(1, firstLetterLover);
+    std::cout << "Your combination: " << combination << std::endl;
+}
--- 03_syntax_basics_ctd/assignment/assignment_03_anna/main.cpp
+++ 03_syntax_basics_ctd/assignment/assignment_03_anna/main.cpp
@@ -7,29 +7,15 @@
 #include <thread>
 #include <chrono>
 
-//#include "ascii.hpp"
+#include "ascii.hpp"
 //#include "mail.hpp"
 
 using namespace std;
 
-// Check which operating system the application is running
 
 int main() {
-    std::string username;
-    std::cout << "What's your name?";
-    std::cin >> username;
-
-    char firstLetterUsername = username[0]; // Accessing the first character
-
-    std::string lover;
-    std::cout << "What's the name of your lover?";
-    std::cin >> lover;
-
-    char firstLetterLover = lover[0];
-
-    // Create strings from individual characters
-    std::string combination = std::string(1, firstLetterUsername) + "+" + std::string(1, firstLetterLover);
-    std::cout << "Your combination: " << combination << std::endl;
+   
+    lettersCombination();
     
     return 0;
 }
